ac6b1ce8d7ecde9e7826cae750a7a7f7298a35a1
DavePearce <david.pearce@ecs.vuw.ac.nz>
2013-04-12 11:57:24 +1200

    Making some initial start

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 # -*-python-*-
 
 import os
+from cherrypy.lib.static import serve_file
+import filedb
 
 # ============================================================
 # Mako Config
@@ -8,37 +10,36 @@ import os
 
 from mako.template import Template
 from mako.lookup import TemplateLookup
-lookup = TemplateLookup(directories=['templates'])
+lookup = TemplateLookup(directories=['html'])
 
 # ============================================================
-# File Downloads
+# Application Entry
 # ============================================================
 
-from cherrypy.lib.static import serve_file
-
-# ============================================================
-# Automark Application
-# ============================================================
-
-class Automark:
+class Main(object):
+    def __init__(self,root_url,username):
+        self.root_url = root_url
+        self.username = username
+        self.data = Data()
+    
     # gives access to images/
     def images(self, filename):
         abspath = os.path.abspath("images/" + filename)
         return serve_file(abspath, "image/png")
-
+    
     # gives access to js/
     def js(self, filename):
         abspath = os.path.abspath("js/" + filename)
         return serve_file(abspath, "application/javascript")
-
+    
     def css(self, filename):
         abspath = os.path.abspath("css/" + filename)
         return serve_file(abspath, "text/css")
-
+    
     # application root
     def index(self):
         template = lookup.get_template("index.html")
-        return template.render()
+        return template.render(ROOT_URL=self.root_url,USER_NAME=self.username)
     
     # exposed
     index.exposed = True
@@ -46,3 +47,9 @@ class Automark:
     js.exposed = True
     css.exposed = True
 
+class Data(object):
+    def __init__(self):
+        #self.permissions = Permissions()
+        #self.permissions.exposed = True
+        self.admin = Permissions()
+        self.admin.exposed = True
