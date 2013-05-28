fbf239ab76a807acb4342d8d9964ab52acc84b4d
DavePearce <david.pearce@ecs.vuw.ac.nz>
2013-05-28 16:09:41 +1200

    Lots of very little tweaks.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 # -*-python-*-
 
 import os
+import os
+import json
+import dircache
+import re
+
 from cherrypy.lib.static import serve_file
 
-import datamodel
-import assignview
+import ecs
 
 # ============================================================
 # Mako Config
@@ -15,6 +19,68 @@ from mako.lookup import TemplateLookup
 lookup = TemplateLookup(directories=['html'])
 
 # ============================================================
+# Data Model
+# ============================================================
+
+class Model(object):
+    def __init__(self,username):
+        self.username = username
+        self.permissions = load("permissions.dat")
+        self.courses = load("courses.dat")
+        self.assignments = {}
+        self.exposed = True
+        
+    # --- Page Handlers ---
+    def coordinating(self):        
+        return json.dumps(join("course",self.courses,select(self.permissions,{"user": self.username, "permission": "coordinator"})))
+    coordinating.exposed = True 
+    
+    def tutoring(self):        
+        return json.dumps(join("course",self.courses,select(self.permissions,{"user": self.username, "permission": "tutor"})))
+    tutoring.exposed = True 
+    
+    def courseconfig(self,course):
+        self.checkPermission(course,"coordinator")        
+        return json.dumps(load(course + "/config.dat"))
+    courseconfig.exposed = True 
+
+    def assignconfig(self,course,assignment):
+        self.checkPermission(course,"coordinator")        
+        return json.dumps(load(course + "/" + assignment + "/config.dat"))
+    assignconfig.exposed = True 
+    
+    def submissions(self,course,assignment):
+        self.checkPermission(course,"coordinator")
+        return json.dumps(ecs.findSubmissions(course,assignment))
+    submissions.exposed = True
+    
+    # --- Query Functions ---
+    def checkPermission(self,course,permission):
+        for p in self.permissions:
+            if p["course"]==course and p["user"] == self.username and p["permission"]==permission:                
+                return
+        raise cherrypy.HTTPError(403,"You do not have permission to access this resource")
+
+# ============================================================
+# Application Views
+# ============================================================
+
+class View(object):
+    def __init__(self,root_url,data):
+        self.root_url=root_url
+        self.data = data
+        self.exposed = True
+        
+    def assignment(self,course,name):
+        self.data.checkPermission(course,"coordinator")
+        template = lookup.get_template("assignmentview.html")
+        return template.render(ROOT_URL=self.root_url,COURSE=course,ASSIGNMENT=name)
+    
+    # exposed stuff
+    assignment.exposed = True 
+
+
+# ============================================================
 # Application Entry
 # ============================================================
 
@@ -22,8 +88,8 @@ class Main(object):
     def __init__(self,root_url,username):
         self.root_url = root_url
         self.username = username
-        self.data = datamodel.Database(username)
-        self.assignview = assignview.Assignment(root_url,self.data)
+        self.data = Model(username)
+        self.view = View(root_url,self.data)
 
     # gives access to images/
     def images(self, filename):
@@ -35,6 +101,7 @@ class Main(object):
         abspath = os.path.abspath("js/" + filename)
         return serve_file(abspath, "application/javascript")
     
+    # gives access to css/
     def css(self, filename):
         abspath = os.path.abspath("css/" + filename)
         return serve_file(abspath, "text/css")
@@ -49,3 +116,36 @@ class Main(object):
     images.exposed = True
     js.exposed = True
     css.exposed = True
+
+# ============================================================
+# Helper functions
+# ============================================================
+
+# Straightforward implementation of select query
+def select(table,matches):
+    results=[]
+    for row in table:
+        m=True
+        for key,match in matches.items():
+            if row[key] != match:
+                m=False
+                break
+        if m:
+            results.append(row)
+    return results
+       
+# Straightforward implementation of join query
+def join(key,table1,table2):
+    results=[]
+    for row1 in table1:
+        for row2 in table2:
+            if row1[key] == row2[key]:
+                results.append(dict(row1.items()+row2.items()))
+    return results
+
+# Load a given file representing a database table.
+def load(filename):
+    f = open("data/" + filename,"r")
+    data = json.load(f)
+    f.close()
+    return data
