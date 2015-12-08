1350691ffec459d8be0982fa44b117a9dbbe641d
DavePearce <david.pearce@ecs.vuw.ac.nz>
2015-12-08 20:29:00 +1300

    Streamlined things a little.
    Have removed a lot of junk, and tried (yet again) to get to a useful
    structure.  Hopefully making some progress.

--- src/main.py
+++ src/main.py
@@ -2,9 +2,7 @@
 
 import os
 from cherrypy.lib.static import serve_file
-import json
-import ecs
-import simpledb
+from cherrypy import request
 
 # ============================================================
 # Mako Config
@@ -14,9 +12,53 @@ from mako.template import Template
 from mako.lookup import TemplateLookup
 lookup = TemplateLookup(directories=['html'])
 
-# ============================================================
-# Application Entry
-# ============================================================
+# ===================================================================
+# Application Resource.  This is a helper class which provides a
+# RESTful resource interface.  Specifically, it allows application
+# objects to be built which respond to GET/PUT requersts, etc in the
+# appropriate fashion.
+# ===================================================================
+
+class Resource(object):
+    # --------------------------------------------------------
+    # Resource Root
+    # --------------------------------------------------------
+    def index(self,**kwargs):
+        if request.method == "GET":
+            return self.GET(kwargs)
+        else:
+            return self.PUT(kwargs)            
+    index.exposed = True
+
+# ===================================================================
+# Application Table.  A table provides a RESTful interface to an
+# underlying database table.
+# ===================================================================
+
+class Table(Resource):
+    
+    def __init__(self,table):
+        # The table is used to identify the database table to which
+        # this resource corresponds.
+        self.table = table
+
+    # -------------------------------------------------------------
+    # Table GET.  Return the contents of this resource, potentially
+    # with a query applied as given by the arguments.
+    # -------------------------------------------------------------
+    def GET(self,kwargs):
+        return "GOT HERE: " + str(kwargs)
+
+    # -------------------------------------------------------------
+    # Table PUT
+    # -------------------------------------------------------------
+    def PUT(self):
+        return "PUT HERE"
+        
+# =================================================================
+# Application Entry.  This is the entry point for this application,
+# and serves all routes.  
+# =================================================================
 
 class Main(object):
     
@@ -28,17 +70,10 @@ class Main(object):
         # This is necessary to limit what the user is and is not
         # allowed to see.
         self.username = username
-        # Always load permissions, since we almost always need that.
-        # We immediately select those permission relevant for this
-        # user, since we don't care about others.
-        self.permissions = select(load("data/permissions.dat"),{"user": self.username})
-        # Create a database handle, which will be passed between
-        # different functions.
-        self.database = simpledb.Database("data/")
 
-    # --------------------------------------------------------
+    # -------------------------------------------------------------
     # Standard resources (images, js, html, etc)
-    # --------------------------------------------------------
+    # -------------------------------------------------------------
     
     # gives access to images/
     def images(self, filename):
@@ -56,9 +91,9 @@ class Main(object):
         return serve_file(abspath, "text/css")
     css.exposed = True
 
-    # --------------------------------------------------------
+    # -------------------------------------------------------------
     # Application Root
-    # --------------------------------------------------------
+    # -------------------------------------------------------------
 
     def index(self):
         template = lookup.get_template("index.html")
@@ -66,122 +101,23 @@ class Main(object):
     index.exposed = True
     # exposed
 
-    # --------------------------------------------------------
-    # Application Views
-    # --------------------------------------------------------
+    # -------------------------------------------------------------
+    # Application View
+    # -------------------------------------------------------------
     
     def view(self,template,course=None,assignment=None):
         template = lookup.get_template(template + ".html")
         return template.render(ROOT_URL=self.root_url,
                                USER=self.username,
                                COURSE=course,       
-                               ASSIGNMENT=assignment,
-                               DATABASE=self.database)        
+                               ASSIGNMENT=assignment)        
     view.exposed = True
 
-    # --------------------------------------------------------
+    # -------------------------------------------------------------
     # Application Data
-    # --------------------------------------------------------
+    # -------------------------------------------------------------
     
-    # Retrieve course configuration data (i.e. settings) which are visible
-    # to the course coordinator.  For example, the list of assignments
-    # and/or tutors is visible to the course coordinator.
-    def course(self,course):
-        # checkPermission(self,course,["coordinator"])        
-        return json.dumps(self.database.course(self.username,course))
-    course.exposed = True
+    courses = Table("courses")
 
-    # Retrieve assignment configuration data (i.e. settings) which is
-    # visible to the course coordinator.  For example, the list of actions 
-    # is visible to the course coordinator.
-    def assignment(self,course,assignment):
-        # checkPermission(self,course,["coordinator"])        
-        return json.dumps(self.database.assignment(self.username,course,assignment))
-    assignment.exposed = True
+ 
 
-    # Retrieve the list of submissions for a given assignment which is
-    # visible to the course coordinator.  This includes the student ID 
-    # and name of each submission, along with a list of the submitted 
-    # files.
-    def submissions(self,course,assignment):
-        # checkPermission(self,course,["coordinator"])        
-        return json.dumps(self.database.submissions(self.username,course,assignment))
-    submissions.exposed = True
-
-    # Retrieve the list of marks for a given assignment submission
-    # which is visible to the course coordinator.  This includes the
-    # student ID and name for each submission, along with the marks
-    # for each completed task.
-    def marks(self,course,assignment):
-        # checkPermission(self,course,["coordinator"])        
-        return json.dumps(self.database.marks(self.username,course,assignment))
-    marks.exposed = True
-
-    # Retrieve the marking sheet data for this assignment, which is 
-    # visible to the tutors.
-    def marksheet(self,course,assignment):
-        # checkPermission(self,course,["tutor"])
-        return json.dumps(self.database.marksheet(self.username,course,assignment))
-    marksheet.exposed = True
-
-    # Run a given task for a given student in a given course and
-    # assignment.  This will produce a mark which is recorded in the
-    # database, along with any supplmentary information (i.e. error
-    # messages).
-    def run(self,course,assignment,task,login):
-        try:
-            return json.dumps(ecs.runTask(course,assignment,login,task))
-        except Exception as e:
-            return str(e) 
-    run.exposed = True
-
-    # --------------------------------------------------------
-    # Authentication
-    # --------------------------------------------------------
-    
-    # Check whether or not the given user has one of a list of
-    # permissions for a given course.  If not, an HTTPError is raised.
-    # For example, we might wish to check that a user has permission
-    # as either "tutor" or "coordinator".  This makes sense if we
-    # assume that a coordinator can always do what a tutor can do.
-    def checkPermission(self,course,permissions):
-        for mp in self.permissions:
-            for rp in permissions: 
-                if mp["course"]==course and mp["permission"]==rp:
-                    return
-        # If we get here, then user doesn't have required permission
-        # so raise HTTP error.
-        raise cherrypy.HTTPError(403,"You do not have permission to access this resource")
-
-# ============================================================
-# Helper functions
-# ============================================================
-
-# Straightforward implementation of select query
-def select(table,matches):
-    results=[]
-    for row in table:
-        m=True
-        for key,match in matches.items():
-            if row[key] != match:
-                m=False
-                break
-        if m:
-            results.append(row)
-    return results
-       
-# Straightforward implementation of join query
-def join(key,table1,table2):
-    results=[]
-    for row1 in table1:
-        for row2 in table2:
-            if row1[key] == row2[key]:
-                results.append(dict(row1.items()+row2.items()))
-    return results
-
-# Load a given file representing a database table.
-def load(filename):
-    f = open(filename,"r")
-    data = json.load(f)
-    f.close()
-    return data
